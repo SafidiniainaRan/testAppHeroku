3111f0749b73f3d407204101fde6cd43c599ba4c
SafidiniainaRan <89478402+SafidiniainaRan@users.noreply.github.com>
2022-01-13 07:23:37 +0300

    first push

---
+++ src/main/java/com/example/testappheruku/web/TestRest.java
@@ -0,0 +1,12 @@
+package com.example.testappheruku.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRest {
+    @GetMapping(path = "/")
+    public String bonjour(){
+        return "Bonjour enao";
+    }
+}
